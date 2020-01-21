cd2ddeaf32cfe60709d013a0b92504c34c9670a1
Unknown <zocker1600@posteo.net>
2020-01-21 21:07:15 +0100

    show error when token is empty

--- download_cr.sh
+++ download_cr.sh
@@ -6,7 +6,9 @@ echo "using token for login...."
 # echo token for debugging
 echo "your download token is: $token"
 
+if test -z $token; then
+	echo "ERROR: you need to specify a login token in order to download the latest version!"
+fi
+
 # use the token in order to download the latest version of CR
 curl -H "Authorization: Bearer $token" -X GET https://discovery.crowd-render.com/api/v0/download-addon/$CR_VERSION.zip -o $CR_VERSION.zip
-
-echo "downloaded $CR_VERSION.zip"
