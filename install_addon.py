cf5d9f388ce3c2d38227770d5b0492724e9d39fc
Unknown <zocker1600@posteo.net>
2020-01-23 17:46:37 +0100

    fix old python calls

--- install_addon.py
+++ install_addon.py
@@ -3,7 +3,7 @@ import bpy
 
 version = "/CR/" + os.environ["CR_VERSION"] + ".zip"
 
-bpy.ops.preferences.addon_install(overwrite=True, filepath=version)
-bpy.ops.preferences.addon_enable(module='crowdrender')
+bpy.ops.wm.addon_install(overwrite=True, filepath=version)
+bpy.ops.wm.addon_enable(module='crowdrender')
 bpy.ops.wm.save_userpref() 
 exit()
