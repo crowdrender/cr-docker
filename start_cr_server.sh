69e6422d73fd01e73388f040fedae13d1e0262fc
zocker-160 <zocker1600@posteo.net>
2021-05-18 17:47:46 +0200

    merge CPU and GPU start script

--- start_cr_server.sh
+++ start_cr_server.sh
@@ -1,35 +1,39 @@
 #! /bin/bash
 # by zocker1600 2019 - 2021
 
-download_cr() {
-	#check if all ENV variables are set
-#	if [ -z "$username" ] || [ -z "$password" ];then
-#		echo "you need to specify username and password!"
-#		exit 1
-#	else
+setup_cr() {
+	# check if crowdrender addon is already downloaded
+	if test -r "/CR/$CR_VERSION.zip"; then
+		echo ""
+		echo ""
+		echo ""
+		echo "-------------------------------------------------------------"
+		echo "found already existing $CR_VERSION.zip"
+	else
 		# download the specified version of the crowdrender addon
 		echo "downloading version $CR_VERSION of crowdrender......"
 		./download_cr.sh
-		
-		# install addon into blender
-		echo "installing crowdrender addon......."
-		/usr/local/blender/blender -b -noaudio --python ./install_addon.py
-		
-		# show the installed version of blender for debugging purposes
-		echo "blender launch test:"
-		/usr/local/blender/blender -b --version
-#	fi
+	fi
+	
+	# install addon into blender
+	echo "installing crowdrender addon......."
+	/usr/local/blender/blender -b -noaudio --python ./install_addon.py
+	
+	# show the installed version of blender for debugging purposes
+	echo "blender launch test:"
+	/usr/local/blender/blender -b --version
 }
-
-# check if machine_uuid is set
-if [ -z "$machine_uuid" ];
-then
-	echo "machine_uuid is not set...generating new unique UUID....."
-	machine_uuid=$(cat /proc/sys/kernel/random/uuid)
-fi
 
 start_server() {
 	# start the CR server
+	if [ $usegpu == "true" ]; then
+		# activate all GPUs (this is a workaround for a bug)
+		echo "activating all available GPUs......"
+		/usr/local/blender/blender -b -noaudio --python activate_gpu.py
+	else
+		echo "INFO: running in CPU only mode!"
+	fi
+	
 	if [ $local == "true" ]; then
 		echo "starting crowdrender server in LOCAL MODE....................."
 		/usr/local/blender/blender -b -noaudio --python /root/.config/blender/$BLENDER_VERSION/scripts/addons/crowdrender/src/py_3_7/serv_int_start.py -- -t "server_int_proc"
@@ -39,16 +43,12 @@ start_server() {
 	fi
 }
 
-
-# check if crowdrender addon is already installed
-if test -r "$CR_VERSION.zip"; then
-	echo ""
-	echo ""
-	echo ""
-	echo "-------------------------------------------------------------"
-	echo "crowdrender is already installed"
-	start_server
-else
-	download_cr
-	start_server
+# check if machine_uuid is set
+if [ -z "$machine_uuid" ];
+then
+	echo "machine_uuid is not set...generating new unique UUID....."
+	machine_uuid=$(cat /proc/sys/kernel/random/uuid)
 fi
+
+setup_cr
+start_server
