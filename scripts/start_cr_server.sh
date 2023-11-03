5b7d87bee0a9749edc0a35ce71b560e639902e78
Jeducious <jamesharrycrowther@gmail.com>
2023-11-03 13:10:29 +1100

    Update to support V0.6.4 CR addon
    The new V0.6.4 and beyond will reequire a change to the commands used to start a server/worker.
    
    Also, the var "use_local_cr" is now false by default and I've made some effort to consolidate and tidy up.

--- scripts/start_cr_server.sh
+++ scripts/start_cr_server.sh
@@ -1,56 +1,47 @@
 #! /bin/bash
 # (c) Crowdrender 2019 - 2022
 
+BLENDER_ADDONS_PATH=/root/.config/blender/$BL_VERSION_SHORT/scripts/addons
+TRUE=true
+FALSE=false
+
 function download_cr {
 
 
-    # exit early if a docker bind mount is specified, but no version number is provided.
-    if [ -z "$local_cr_path" ] && [ -z "$cr_version" ]; then
-        echo "ERROR: specifying local_cr_path requires you to also specify the version by setting the cr_version option."
-        exit 1
+    # If no token is provided and the user wishes to download the install package...
+    if [ -z $token ]; then
 
-    # if the token is not provided and neither has a local bind mount, then exit early.
-    elif [ -z "$local_cr_path" ] && [ -z $token ]; then
-
+        # Signal an error, its not possible to download without auth.
         echo "ERROR: you need to specify a login token in order to download the latest version!"
         exit 1
-
-    fi
-
-    if [ -z "$cr_version" ]; then
-        echo "No specific version set, defaulting to 'latest'."
-        cr_version="latest"
-    fi
-
-    # if the user provides a local path 
-    if ! [ -z "$local_cr_path" ]; then
-        
-        # use the provided path as the source of the addon package instead of downloading it
-        echo "Going to use $local_cr_path as the source of the addon package."
-
-    # alternatively, if the request version is the latest one...
-    elif [ $cr_version == "latest" ]; then
-
-        # use the token for downloading the addon package useing the dedicated 'latest' URL
-        echo "downloading latest CR version!"
-        curl -# -H "Authorization: Bearer $token" -X GET https://discovery.crowd-render.com/api/download/addon/$cr_version -o $cr_version.zip
     
-    # otherwise, use the provided version number.
     else
-        # 
-        echo "downloading CR version: $cr_version!"
-        curl -# -H "Authorization: Bearer $token" -X GET https://discovery.crowd-render.com/api/download/addon/$cr_version.zip -o $cr_version.zip
+        echo "About to download the $cr_version version of the addon."
+
+        # ... use the token for downloading the addon package useing the dedicated 'latest' URL
+        curl -# -H "Authorization: Bearer $token" -X GET https://discovery.crowd-render.com/api/download/addon/$cr_version -o $cr_version.zip
+
     fi
 }
+
 
 function setup_cr {
     
     # check to see if the local dev path is set, don't install the
     # addon from discovery if so
-    if ! [ -z "$local_cr_path" ];
+    if [ $use_local_cr = $TRUE ];
     then
-        echo "using mounted location for CR addon"
-        /usr/local/blender/blender -b -noaudio --python ./install_addon.py -- -e
+        
+        # make sure the script path is set, its not going to be referenced by Blender unless the path is added
+        # if we're using the source code for the addon on the host, then we need to setup the script paths properly
+        add_script_location="import bpy
+bpy.ops.preferences.script_directory_add(directory=\"$BLENDER_ADDONS_PATH\")
+bpy.ops.wm.save_userpref()"
+
+        echo " executing $add_script_location"
+
+        /usr/local/blender/blender -b -noaudio --python-expr "$add_script_location" #blender needs a restart after adding a script location.
+        /usr/local/blender/blender -b -noaudio --python ./install_addon.py
     
     # check if crowdrender addon is already downloaded
     elif test -r "/CR/$cr_version.zip"; then
@@ -61,15 +52,14 @@ function setup_cr {
         echo "found already existing $cr_version.zip"
         # install addon into blender
         echo "installing crowdrender addon......."
-        /usr/local/blender/blender -b -noaudio --python ./install_addon.py -- -i -f "/CR/$cr_version.zip"
+        /usr/local/blender/blender -b -noaudio --python ./install_addon.py -i "/CR/$cr_version.zip"
 
     else
-        # download the specified version of the crowdrender addon
-        echo "downloading version $cr_version of crowdrender......"
+
         download_cr
 
-        echo "installing crowdrender addon......."
-        /usr/local/blender/blender -b -noaudio --python ./install_addon.py -- -i -f "/CR/$cr_version.zip"
+        echo "installing crowdrender addon from /CR/$cr_version.zip"
+        /usr/local/blender/blender -b -noaudio --python ./install_addon.py -i "/CR/$cr_version.zip"
     fi
 
     
@@ -79,15 +69,48 @@ function setup_cr {
 }
 
 function start_server {
-    ROOT_PATH="/root/.config/blender/$BL_VERSION_SHORT/scripts/addons/crowdrender"
+
+    CR_ADDON_ROOT="$BLENDER_ADDONS_PATH/crowdrender"
+
+    if test -d $CR_ADDON_ROOT; then
+        echo "Crowdrender found at $CR_ADDON_ROOT"
+    else
+        echo "OOPS! Couldn't find Crowdrender at $CR_ADDON_ROOT"
+    fi
 
     # test if the path to the addon actually exists. 
-    if test -f "$ROOT_PATH/cr_source/core/serv_int_start.py"; then
-        START_PY_FILE="$ROOT_PATH/cr_source/core/serv_int_start.py"
+    if test -f "$CR_ADDON_ROOT/cr_source/core/serv_int_start.py"; then
+        START_PY_FILE="$CR_ADDON_ROOT/cr_source/core/serv_int_start.py"
+    elif test -f "$CR_ADDON_ROOT/cr_core/serv_int_start.py"; then
+        START_PY_FILE="$CR_ADDON_ROOT/cr_core/serv_int_start.py"
+    else
+        echo "The path for Crowdrender's scripts was not found, tried using $CR_ADDON_ROOT"
+        echo "doing an ls -al on the selected location for the root path"
+        echo "user is"
+        whoami
+        cd "/root/.config/blender/$BL_VERSION_SHORT"
+        ls -al
     fi
+
+    # check if there's a blender_process script, if so use this instead of the other method of starting the addon.
+    if test -f "$CR_ADDON_ROOT/bootstrap/blender_process.py"; then
+        /usr/local/blender/blender \
+        -b \
+        -noaudio \
+        --python "$CR_ADDON_ROOT/bootstrap/blender_process.py"\
+        -- \
+        --addon-dir "$CR_ADDON_ROOT" \
+        --type server_interface \
+        -p "$persistent" \
+        -ct "$token" \
+        --override-machine-uuid "$machine_uuid" \
+        -ak "$secret"
+
+    fi 
 
     # start blender with the proper arguments to start a headless server	
     echo "starting crowdrender server using version $cr_version ..."
+    echo "Running script to start server $START_PY_FILE"
     /usr/local/blender/blender -b -noaudio --python $START_PY_FILE -- -t "server_int_proc" -p "$persistent" -ct "$token" --override-machine-uuid "$machine_uuid" -ak "$secret"
     
 }
